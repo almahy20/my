75268ccb42faa5b1c246f2d2e60e976016381ec2
almahy20 <157045962+almahy20@users.noreply.github.com>
2024-02-15 07:38:11 -0800

    Add files via upload

---
+++ js/my.js
@@ -0,0 +1,7 @@
+let menu = document.getElementById("menu")
+let links = document.getElementById("#links")
+
+menu.onclick = function(){
+menu.classList.toggle('fa-times');
+link.classList.toggle('active')
+}
